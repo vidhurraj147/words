dfc8d21e1a8d289af1c9005ecad8374439f2d099
vidhurraj147 <vidhurraj147@gmail.com>
2018-04-02 14:58:31 +0000

    Hello world progm

---
+++ src/main/java/training/maven/quick/HelloWorld.java
@@ -0,0 +1,11 @@
+package training.maven.quick;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is HelloWorldJAVA words");
+
+	}
+
+}
